dcc99c6a29f31d725eedb7b0cbf78d2be763daad
berggren00 <andreas_berggren@hotmail.com>
2024-10-14 23:09:05 +0200

    Added comments

--- DiceGame.java
+++ DiceGame.java
@@ -1,7 +1,6 @@
-
-
 public class DiceGame {
-    public static void main(String[] args) {
+    
+    public static void main(String[] args) {        // Main funktion där vi skapar ett objekt av GameEngine och anropar start-metoden
         GameEngine engine = new GameEngine();
         engine.start();
     }
--- GameEngine.java
+++ GameEngine.java
@@ -2,40 +2,53 @@ import java.util.Scanner;
 
 public class GameEngine {
 
-    public void start() {
+    // Metod som startar spelet
+    public void start() { 
         Scanner scan = new Scanner(System.in);
 
-        System.out.println("How many rounds?: ");
+        // Frågar användaren hur många rundor de vill spela
+        System.out.println("How many rounds do you want to play?: ");
         int rounds = scan.nextInt();
 
-        System.out.println("How many sides?: ");
+        // Frågar användaren hur många sidor de vill spela med
+        System.out.println("How many sides do you want to play with?: ");
         int sides = scan.nextInt();
 
+        // SKapar en ny tärning med antalet sidor användaren valt
         Die die = new Die(sides);
 
 
-        System.out.println("What is your name? :");
+        // Frågara användaren om vilket namn de vill använda
+        System.out.println("What is your name?: ");
         String name = scan.next();
 
+        // Skapar en ny spelare med angivet namn och sidor på tärnignen
         Player player = new Player(name, die);
         System.out.println("Welcome " + player.getName());
+        
+        // Loop genom varje runda
         for (int i = 0; i < rounds; i++) {
-            System.out.println("What number do you predict: ");
+            System.out.println("What number do you predict: ");     // Användaren gissar ett tal
             int playerInput = scan.nextInt();
             player.rollDice();
 
             int dieValue = player.getDieValue();
-            System.out.println("Player " + player.getName() + " rolled a: " + dieValue);
+            System.out.println("Player " + player.getName() + " rolled a: " + dieValue);    // Printar vad användaren rullade
 
+            // Ökar användarens poäng om hen gissat korrekt värde
             if (playerInput == dieValue) {
                 player.increaseScore();
-                System.out.println("Player " + player.getName() + " won and got 1 point");
-                System.out.println("Current score: " + player.getScore());
+                System.out.println("Player " + player.getName() + " guessed correctly and got 1 point!");
+                System.out.println("Current score: " + player.getScore());  // Printar "current score"
             } else {
-                System.out.println("You lost");
+                System.out.println("Unfortunately, you did not guess the correct number, which means you lost this round. :(");
             }
-
+            
         }
+        // Skriver ut totalpoäng efter avslutat "game"
+        System.out.println("Game over!");
+        System.out.println("Player " + player.getName() + " got a score of: " + player.getScore());
+        
         scan.close();
     }
 
